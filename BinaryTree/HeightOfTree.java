32e3a4799bb8903954acdf24affa8a619877db78
Vaibhav <vaibhavmittal2017@gmail.com>
2024-10-29 14:38:46 +0530

    binary tree 3

--- BinaryTree/HeightOfTree.java
+++ BinaryTree/HeightOfTree.java
@@ -147,6 +147,120 @@ public class HeightOfTree {
         }
         System.out.println();
     }
+    public static void kLevel(Node root,int level,int k){
+        if(root == null){
+            return;
+        }
+        if(level == k){
+            System.out.print(root.data+" ");
+            return;
+        }
+        kLevel(root.left, level+1, k);
+        kLevel(root.right, level+1, k);
+    }
+    public static boolean getPath(Node root,int n,ArrayList<Node> path){
+        if(root == null){
+            return false;
+        }
+        path.add(root);
+
+        if(root.data == n){
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if(foundLeft || foundRight){
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+    }
+    public static Node lca(Node root,int n1,int n2){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root,n1,path1);
+        getPath(root,n2,path2);
+
+        //Last Common Ancestor
+        int i = 0;
+        for(;i<path1.size() && i<path2.size();i++){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+        }
+        //Last Equal Node -> (i-1)th
+        Node lca = path1.get(i-1);
+        return lca;
+    }
+    public static Node lca2(Node root,int n1,int n2){//O(n)
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        //leftLca = val rightLca = null
+        if(rightLca == null){
+            return leftLca;
+        }
+        if(leftLca == null){
+            return rightLca;
+        }
+        return root;
+    }
+    public static int lcaDist(Node root,int n){
+        if(root == null){
+            return -1;
+        }
+        if(root.data == n){
+            return 0;
+        }
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right,n); 
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        }
+        else if(leftDist == -1){
+            return rightDist+1;
+        }
+        else{
+            return leftDist+1;
+        }
+    }
+    public static int minDist(Node root,int n1,int n2){
+        Node lca = lca2(root, n1, n2);
+
+        int dist1 = lcaDist(lca,n1);
+        int dist2 = lcaDist(lca,n2);
+
+        return dist1 + dist2;
+    }
+    public static int transform(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+        root.data = newLeft + leftChild + newRight + rightChild;
+        return data;
+    }
+    public static void preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
     public static void main(String[] args) {
         /*
                         1
@@ -181,5 +295,20 @@ public class HeightOfTree {
         System.out.println(diameter(root).diam);
         System.out.println(isSubtree(root, subRoot));
         topView(root);
+        int k = 3;
+        kLevel(root, 1, k);
+        int n1 = 4, n2 = 5;
+        System.out.println(lca(root, n1, n2).data);
+        System.out.println(lca2(root, n1, n2).data);
+        System.out.println(minDist(root, n1, n2));
+        /* Expected Sun tree :
+                        27
+                       / \
+                      9   13
+                     / \ / \
+                    0  0 0  0
+        */
+        transform(root);
+        preorder(root);
     }
 }
